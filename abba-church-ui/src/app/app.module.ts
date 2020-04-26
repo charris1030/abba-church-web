addac8ec9ce56aad77eeab4914c6c9a92f74a91d
Matheus Harris <mpharris1020@gmail.com>
2020-04-25 20:17:01 -0400

    language pages

--- abba-church-ui/src/app/app.module.ts
+++ abba-church-ui/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { MailService } from './services/mail.service';
 import { AccountComponent } from './account/account.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthenticationService,
