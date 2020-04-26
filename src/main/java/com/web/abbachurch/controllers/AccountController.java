745fa09d5c2ebd53ba23e7d622089fbbe71a44af
Christopher Harris <cghar1030@brandeis.edu>
2020-04-26 12:26:24 -0400

    #create account dao

--- src/main/java/com/web/abbachurch/controllers/AccountController.java
+++ src/main/java/com/web/abbachurch/controllers/AccountController.java
@@ -1,12 +1,22 @@
 package com.web.abbachurch.controllers;
 
+import com.web.abbachurch.dao.AccountDAO;
 import com.web.abbachurch.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
 @CrossOrigin("*")
 public class AccountController {
+
+    private static final Logger log = LoggerFactory.getLogger(AccountController.class);
+
+
+    @Autowired
+    private AccountDAO accountDAO;
 
     /**
      * CH - creates new Abba Church Account
@@ -17,11 +27,19 @@ public class AccountController {
             produces = "application/json", consumes = "application/json")
     public String sendMail(@RequestBody User user) {
 
-        System.out.println("Creating new Abba Church Account for User: "+ user);
+        log.info("Creating new Abba Church Account for User {}", user);
 
         String responseStr;
+        int createUserResp;
 
         responseStr = "Abba Church Account Creation Succeeded";
+
+        try {
+            createUserResp = accountDAO.insert(user);
+            log.info("Create User Response {}", createUserResp);
+        } catch (Exception e) {
+            log.error("Error when creating user account: ", e.getCause());
+        }
 
         return responseStr;
     }
