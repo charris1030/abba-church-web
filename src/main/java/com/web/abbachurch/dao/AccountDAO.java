bd35a392c32ec69c67529812b183ffcefaedd95a
Christopher Harris <cghar1030@brandeis.edu>
2020-04-29 20:43:29 -0400

    #mysql and abba church DB

--- src/main/java/com/web/abbachurch/dao/AccountDAO.java
+++ src/main/java/com/web/abbachurch/dao/AccountDAO.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,11 +17,19 @@ public class AccountDAO {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public AccountDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public int insert(User user) {
-        return jdbcTemplate.update(
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int newRecordId = 0;
+        jdbcTemplate.update(
                 "insert into abba_church_users (user_name, pass_word, first_name, last_name, phone_number) " +
                         "values(?,?,?,?,?)",
                 user.getUserName(), user.getPassWord(), user.getFirstName(), user.getLastName(), user.getPhoneNumber());
+        log.info("ID for new account record: {}", newRecordId);
+        return newRecordId;
     }
 
 }
